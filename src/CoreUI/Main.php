91216bdc7f4d62c259425c962605f240ca57365f
WarTechDevs <44534195+WarTechDevs@users.noreply.github.com>
2018-10-29 20:30:53 -0400

    Update Main.php

--- src/CoreUI/Main.php
+++ src/CoreUI/Main.php
@@ -4,12 +4,7 @@ use jojoe77777\FormAPI;
 
 class Main extends PluginBase implements Listener{
     
-    public function onEnable(){
-        $this->getLogger()->info("CoreUI By WarTechDevs Enabled");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        }
-    }
-    public function onDisable(){
-        $this->getLogger()->info("CoreUI By WarTechDevs Disabled");
-    }
-}
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents(($this), $this);
+		$this->getLogger()->info("Plugin Enabled By WarTechDevs");
+	}
