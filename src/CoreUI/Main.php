43f6461cd4741efae36afd4efcbc4fb8e2e18a2d
WarTechDevs <44534195+WarTechDevs@users.noreply.github.com>
2018-11-01 02:19:40 -0400

    Update Main.php

--- src/CoreUI/Main.php
+++ src/CoreUI/Main.php
@@ -1,1 +1,10 @@
-
+<?php
+namespace CoreUI;
+use pocketmine\plugin\PluginBase;
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents(($this), $this);
+		$this->getLogger()->info("Plugin Enabled By WarTechDevs");
+	}
+}
